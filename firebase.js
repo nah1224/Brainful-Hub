7255427cd2d30dae9ff50dddaee2f7ad44800f94
nah1224 <nahindez28@gmail.com>
2026-02-10 16:45:14 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA6AS1Nztjj0KWdtOxwPpxt0dIWrvZQgQo",
   authDomain: "brainful-hub.firebaseapp.com",
@@ -18,4 +16,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// 🔹 Initialize Firebase Auth (for login)
+export const auth = getAuth(app);
+
+// 🔹 Initialize Firestore (for exams, results)
+export const db = getFirestore(app);
