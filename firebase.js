1b352023a30577aa468cb84ed7d8209d3a83a626
nah1224 <nahindez28@gmail.com>
2026-02-10 16:22:48 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD-EXAMPLEKEY1234567890",
-  authDomain: "brainful-hub.firebaseapp.com",
-  projectId: "brainful-hub",
-  storageBucket: "brainful-hub.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcdef123456"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdef123456"
 };
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
